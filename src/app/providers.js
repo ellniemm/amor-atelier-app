be10123391c35a0381da035146dba38b1ef23c2d
ellniemm <adelweis876@gmail.com>
2026-09-21 18:54:18 +0700

    update 21-9-2026

--- src/app/providers.js
+++ src/app/providers.js
@@ -1,7 +1,18 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { Agentation } from "agentation";
 
 export default function Providers({ children }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      {children}
+      {process.env.NODE_ENV === "development" && (
+        <Agentation
+          appName="AMOR ATELIEER"
+          endpoint="http://localhost:4747"
+        />
+      )}
+    </SessionProvider>
+  );
 }
